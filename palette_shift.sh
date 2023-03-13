a5870408f5a61292028e1f03e3ac6247e1f33b70
Neil Johnson <neil@cadent.com>
2023-03-13 11:11:36 -0400

    Add new color palette

--- palette_shift.sh
+++ palette_shift.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 # convert all colors in the database from one palette to another
-currentcolors=("#29579d" "#5a82b6" "#8aaccf" "#91ceb4" "#97ef99")
-newcolors=("#1d5a76" "#2a7fa6" "#94bfd2" "#5d9c78" "#a2d984")
+currentcolors=('#29579d' '#5a82b6' '#8aaccf' '#91ceb4' '#97ef99')
+newcolors=('#216584' '#69a5c0' '#ffc857' '#63b34f' '#a1d195')
 
 length=${#currentcolors[@]}
 
 for (( i=0; i < length; i++ ))
 do
     uppercolor=$( echo "${currentcolors[$i]}" | tr '[:lower:]' '[:upper:]' )
-    echo "Replacing ${uppercolor} with ${newcolors[$i]}..."
+    echo "Replacing ${uppercolor} with ${newcolors[$i]} ..."
     # wp search-replace "${uppercolor}" "${newcolors[$i]}"
     
     lowercolor=$( echo "${currentcolors[$i]}" | tr '[:upper:]' '[:lower:]' )
-    echo "Replacing ${lowercolor} with ${newcolors[$i]}..."
-    # wp search-replace "${uppercolor}" "${newcolors[$i]}"
+    echo "Replacing ${lowercolor} with ${newcolors[$i]} ..."
+    # wp search-replace "${lowercolor}" "${newcolors[$i]}"
 done
 
 echo "Replacements complete."
